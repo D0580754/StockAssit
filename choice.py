64fdbe659b1b64e735639b75f0613044e800ddb8
D0580754 <32447524+D0580754@users.noreply.github.com>
2019-11-11 14:59:05 +0800

    Add files via upload

--- choice.py
+++ choice.py
@@ -1,7 +1,8 @@
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
+import json
+import pandas as pd
 
 def techface(name):
     html = urlopen('http://tw.stock.yahoo.com/d/i/rank.php?t='+name+'&e=tse&n=5')
@@ -16,8 +17,6 @@ def techface(name):
     c = '\n'.join(b)
 
     return c
-
-
 def chipface(name):
     html = urlopen('https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/'+name+'.djhtm')
     soup = BeautifulSoup(html)
@@ -32,3 +31,35 @@ def chipface(name):
 
     return c
 
+def basicface(name):
+        #最近一個月營收創新高
+    html = urlopen('https://concords.moneydj.com/z/zk/zk2/'+name+'.djhtm')
+    soup = BeautifulSoup(html)
+    a=[]
+    for i in soup.find_all('td',class_='zkt1L'):
+        a.append(i.a.text)
+
+    c = '\n'.join(a)
+    
+    return c
+def EPSBPR():
+    date = '20191108'
+    html = urlopen('http://www.tse.com.tw/exchangeReport/BWIBBU_d?response=json&date='+date+'&selectType=ALL')
+    jcontent = json.loads(html.read())
+    data = jcontent['data']
+    data = [i for i in data if i[4]!='-']
+    Dividend_list = sorted(data , key=lambda x: float(x[2].replace(',','')),reverse=True)[:100]
+    print(Dividend_list)
+    #columns
+    df = pd.DataFrame(jcontent['data'])
+    df.columns = ['證券代號','證券名稱','殖利率(%)','股利年度','本益比',
+                    '股價淨值比','財報年/季']
+    PBR = pd.to_numeric(stockdf['股價淨值比'], errors='coerce') < 0.7 # 找到股價淨值比小於0.7的股票
+    EPS = pd.to_numeric(stockdf['本益比'], errors='coerce') < 10 # 找到本益比小於13的股票
+    candidate= stockdf[(PBR & EPS)]
+    a=[]
+    for i in candidate['證券名稱']:
+        a.append(i)
+
+    c = '\n'.join(a)
+    return c
